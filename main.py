bdc67f4eab500f93a289d98fe54a2ddbf3ad0164
Yoly Keys <116820221+codewithkeys@users.noreply.github.com>
2023-01-02 17:34:11 +0000

    Instead of using the input(). Now, make the checkDriverAge() function accept an argument of age, so that if I enter:
    checkDriverAge(92);
    it returns "Powering On. Enjoy the ride!"
    also make it so that the default age is set to 0 if no argument is given.

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-# age = input("What is your age?: ")
+age = input("What is your age?: ")
+
+def checkDriverAge():
+    if int(age) < 18:
+	       print("Sorry, you are too young to drive this car. Powering off")
+    elif int(age) > 18:
+	       print("Powering On. Enjoy the ride!");
+    elif int(age) == 18:
+	       print("Congratulations on your first year of driving. Enjoy the ride!")
+        
+checkDriverAge()
+
+
 
 def checkDriverAge(age):
     if int(age) < 18:
@@ -9,6 +21,26 @@ def checkDriverAge(age):
 	       print("Congratulations on your first year of driving. Enjoy the ride!")
         
 checkDriverAge(92)
+
+#1. Wrap the above code in a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. 
+# Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+
+#2 Instead of using the input(). Now, make the checkDriverAge() function accept an argument of age, so that if you enter:
+#checkDriverAge(92);
+#it returns "Powering On. Enjoy the ride!"
+#also make it so that the default age is set to 0 if no argument is given.
+
+
+
+# Andrei's solution to the problem
+age = input("What is your age?: ")
+
+if int(age) < 18:
+	print("Sorry, you are too young to drive this car. Powering off")
+elif int(age) > 18:
+	print("Powering On. Enjoy the ride!");
+elif int(age) == 18:
+	print("Congratulations on your first year of driving. Enjoy the ride!")
 
 #1. Wrap the above code in a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. 
 # Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
